76be0e57723d6f022261dcac8273ed512c6ec8d3
Ghislain Leblanc <ghislain.leblanc@purepoint.io>
2018-03-29 15:22:36 -0400

    Navigation between text fields.

--- StrictIDtest/StrictIDtest/LoginViewController.swift
+++ StrictIDtest/StrictIDtest/LoginViewController.swift
@@ -19,12 +19,37 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        usernameTextField.addTarget(self, action: #selector(self.textFieldDidEnd(textField:)), for: UIControlEvents.editingDidEndOnExit)
+        passwordTextField.addTarget(self, action: #selector(self.textFieldDidEnd(textField:)), for: UIControlEvents.editingDidEndOnExit)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func textFieldDidEnd(textField: UITextField) {
+        let text = textField.text
+        if text?.count != 0 {
+            switch textField {
+            case usernameTextField:
+                passwordTextField.becomeFirstResponder()
+            case passwordTextField:
+                passwordTextField.resignFirstResponder()
+                login()
+            default:
+                break
+            }
+        }
+    }
+    
+    private func login() {
+        guard let username = usernameTextField.text, let password = passwordTextField.text, username.count != 0, password.count != 0 else {
+            messageLabel.text  = "Missing login information."
+            return
+        }
+        
+        messageLabel.text = ""
     }
 }
 
